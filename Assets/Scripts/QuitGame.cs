ee3ef36aed1f0802de7b4c8d4cffaf42f1bfa547
Daniel Schulz <faulolio@gmail.com>
2021-01-31 03:01:01 +0100

    Fix build

--- Assets/Scripts/QuitGame.cs
+++ Assets/Scripts/QuitGame.cs
@@ -1,12 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "QuitGame", menuName = "ScriptableObjects/QuitGame", order = 1)]
-public class QuitGame : ScriptableObject
-{
+public class QuitGame : ScriptableObject {
     public void Quit() {
+#if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
+#endif
         Application.Quit();
     }
 }
